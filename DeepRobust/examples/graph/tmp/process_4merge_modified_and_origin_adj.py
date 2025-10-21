43557e6a3a5ed7340241eaa73f394c825f2ccb2e
Jingyuan <jingyuanli353@gamil.com>
2025-10-21 21:52:44 +0800

    10212

--- DeepRobust/examples/graph/tmp/process_4merge_modified_and_origin_adj.py
+++ DeepRobust/examples/graph/tmp/process_4merge_modified_and_origin_adj.py
@@ -17,34 +17,9 @@ idx_test = d.idx_test
 # 读取原文件
 data = np.load('cora.npz', allow_pickle=True)
 
-# 拿到原特征和标签等
-
-# with np.load('/tmp/cora.npz') as loader:
-#     features = sp.csr_matrix((loader['attr_data'],
-#                               loader['attr_indices'],
-#                               loader['attr_indptr']),
-#                               shape=loader['attr_shape'])
-#     labels = loader['labels']  # 标签就是 'labels'
-
-# labels = data['labels']
-# idx_train = data['idx_train']
-# idx_val = data['idx_val']
-# idx_test = data['idx_test']
-
-# # 这里是你新的邻接矩阵 (numpy 或 scipy.sparse 格式都可以)
-# new_adj = sp.csr_matrix(new_adj_dense)   # 建议保持 csr 格式以节约空间
 new_adj = sp.load_npz(os.path.join(REPO_ROOT, 'mod_adj-095.npz')).tocsr()
 new_adj = new_adj.tocsr()
-# new_features = sp.load_npz('DeepRobust/mod_features.npz')
-# new_features = new_features.tocsr()
-# # 重新打包保存
-# np.savez('cora_modified.npz',
-#          adj=new_adj,
-#          features=features,
-#          labels=labels,
-#          idx_train=idx_train,
-#          idx_val=idx_val,
-#          idx_test=idx_test)
+
 features = features.tocsr()
 np.savez('cora_modified_095.npz',
          adj_data=new_adj.data,
